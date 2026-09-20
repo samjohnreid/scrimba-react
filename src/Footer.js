54da0cd69ec9c56ef874b88ce9b787b4212be445
Sam Reid <samuel_reid@premierinc.com>
2026-09-20 17:29:59 -0400

    componentizing and modularizing

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2026 Reid development. All rights reserved.</small>
+        </footer>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
+const root = createRoot(document.getElementById("root"))
+
 import "./index.css";
 
-const root = createRoot(document.querySelector('#root'));
-
-function StaticPage() {
-    return(
-        <section>
-            <img src="react-logo.png" width="40" />
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </section>
-    );
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
 }
 
 root.render(
-    <StaticPage />
-);
+    <Page />
+)
