af6b05beaf9ce4f3688dc4ac6b07ca73cf143e2a
Sam Reid <samuel_reid@premierinc.com>
2026-09-21 16:36:18 -0400

    setting up project

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <small>© 2026 Reid development. All rights reserved.</small>
-        </footer>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import { createRoot } from "react-dom/client"
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
-const root = createRoot(document.getElementById("root"))
-
+import { createRoot } from 'react-dom/client';
 import "./index.css";
+import App from './App.js';
 
-function Page() {
-    return (
-        <>
-            <Header />
-            <MainContent />
-            <Footer />
-        </>
-    )
-}
+const root = createRoot(document.querySelector('#root'));
 
-root.render(
-    <Page />
-)
+root.render(<App />);
