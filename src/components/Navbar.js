512b5b98b74b670a16a9be4bd385921dc80b4704
Sam Reid <samuel_reid@premierinc.com>
2026-09-23 11:28:21 -0400

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar() {
         <header className="header">
             <nav className="header__nav">
                 <img className="header__logo" src="react-logo.png" alt="REact logo" />
-                <span className="header__text">ReactFacts</span>
+                <span className="header__text">Data Driven React!</span>
             </nav>
         </header>
     )
